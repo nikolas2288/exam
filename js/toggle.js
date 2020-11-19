075df53ba6a0e8b3e3bdd6dd2ec9b5b005034a33
nikolas228 <kasiva232@gmail.com>
2020-11-19 18:58:37 +0200

    catalog + product

---
+++ js/toggle.js
@@ -0,0 +1,17 @@
+// var imgToggle = document.querySelectorAll('.popup--img')
+var acc = document.querySelectorAll(".popup--title");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+console.log(acc);
